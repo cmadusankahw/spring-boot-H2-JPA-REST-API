86caf80cf23bc0226da3c3f1f70b567817e37940
Chiran Hewawitharana <chiranh@zone24x7.com>
2020-11-01 07:35:12 +0530

    added initial exception handler classes and completed Student entity

---
+++ src/main/java/com/zon24x7/technical/induction/chiran/hewawitharana/studentManagement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.zon24x7.technical.induction.chiran.hewawitharana.studentManagement.exception;
+
+public class GlobalExceptionHandler {
+}
--- src/main/java/com/zon24x7/technical/induction/chiran/hewawitharana/studentManagement/model/Student.java
+++ src/main/java/com/zon24x7/technical/induction/chiran/hewawitharana/studentManagement/model/Student.java
@@ -1,8 +1,6 @@
 package com.zon24x7.technical.induction.chiran.hewawitharana.studentManagement.model;
 
 import javax.persistence.Entity;
-import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,31 +10,51 @@ import java.util.Date;
 @Entity
 @Table(name = "student")
 public class Student {
-    private  final Long id;
-    private  final String name;
-    private  final Date dob;
-    private final String address;
+    private Long id;
+    private String name;
+    private Date dob;
+    private String address;
 
-    public Student(Long id, String name, Date dob, String address) {
-        this.id = id;
+    public Student(String name, Date dob, String address) {
         this.name = name;
         this.dob = dob;
         this.address = address;
+    }
+
+    public Student() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long Id) {
+        this.id = Id;
     }
 
     public String getName() {
         return name;
     }
 
-    public Long getId() {
-        return id;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Date getDob() {
         return dob;
     }
 
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
     public String getAddress() {
         return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
